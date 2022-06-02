8940d7a5925ccf065de04ad804ce61c307b65f6e
edumes <66120423+edumes@users.noreply.github.com>
2022-06-02 12:10:11 -0300

    att

---
+++ operadores.js
@@ -0,0 +1,22 @@
+// Operadores Aritmeticos
+let salario = 100;
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(5 ** 5);
+
+let idade = 15;
+
+console.log(++idade);
+console.log(idade--);
+
+// Operadores de atribuição
+
+
+
+// Operadores de comparação
+// Operadores logicos
+// Operadores Bitwise
+
---
+++ tipo_de_funcoes.js
@@ -0,0 +1,16 @@
+//realiza uma tarefa, mas não retorna nada
+function dizerNome(){
+    console.log('Eduardo')
+};
+
+dizerNome();
+//faz um calculo ou operaçao e retona algo 
+function MultiplicarPorDois(valor){
+    return valor * 2;
+};
+
+//console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+
+console.log(resultado);
