75eee894dcb6f8a61d4d4c8f75829a5a140bef2e
edumes <66120423+edumes@users.noreply.github.com>
2022-06-06 14:28:08 -0300

    Update operadores.js

--- operadores.js
+++ operadores.js
@@ -1,22 +1,19 @@
-// Operadores Aritmeticos
+//-------aritimetico---------------
 let salario = 100;
-
 console.log(salario + salario);
 console.log(salario - salario);
 console.log(salario * salario);
 console.log(salario / salario);
 console.log(5 ** 5);
-
+//-------atribuição-----------------
 let idade = 15;
-
 console.log(++idade);
 console.log(idade--);
-
-// Operadores de atribuição
-
-
-
-// Operadores de comparação
-// Operadores logicos
-// Operadores Bitwise
-
+//--------igualdade-----------------
+console.log(1 === 1);
+console.log('1' === 1);
+//--------ternario------------------
+//100 pontos = comum; 200 pontos = premium;
+let pontos = 100;
+let tipo = pontos > 100 ? 'premium' : 'comum';
+console.log(tipo);
